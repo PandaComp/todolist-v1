b882821c4d9bfbe1778bbcb25fabdb0fa8e37e15
PandaComp <pantonio2000@gmail.com>
2022-05-07 12:51:18 -0400

    Fix opertor bug

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
-const x = new Date()
-console.log(x.getHours())
+
 app.get("/", (req, res) => {
     const today = new Date()
-    if ((today.getDay() === 6 || today.getDay() === 0) || (today.getHours() <= 9 || today.getHours() >= 17)) {
+ if ((today.getDay() === 6 || today.getDay() === 0) || (today.getHours() < 9 || today.getHours() >= 17)) {
         res.send("Closed")
     } else {
         res.send("Open")
     }
-       
 })
 
 app.listen(3000, ()=>{
